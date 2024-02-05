70a1d090a40dbecf62cc44b32838a58cd5b0f175
SametDemiray <142938487+SametDemiray@users.noreply.github.com>
2024-02-05 09:57:10 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,12 @@ package org.example;
 
 import java.util.Scanner;
 
-// Klavyeden girilen kelimeyi belirtilen anahtarlara göre "Affine Şifreleme (Affine Cipher)" algoritmasıyla şifreleyen program
+// Klavyeden girilen kelimeyi belirtilen anahtarlara göre "Affine Şifreleme (Affine Cipher)" algoritmasıyla şifreleyen program.
 // Her harf ( ax + b ) mod 26 fonksiyonuyla şifrelenir ; burada b, kaymanın büyüklüğüdür.
 // Bu şifreleme yöntemindeki amaç geometride doğrunun denklemi olarak bilinen y=ax+b doğrusal fonksiyonunu şifreleme işleminde kullanmaktır.
 // Orn. İngiliz Alfabesini(26 harf) Kullanarak "Bursa" ismini a=5 ve b=1 anahtarıyla şifrelenmesi
 // Sonuc = Bursa --> (a) --> 5 (b) --> 1 Şifreli Kelime ---->>> GXINB
-// Java -5-1-> UBCB
+//   "   = Java -5-1-> UBCB
 public class Main {
     public static void main(String[] args) {
 
